34cbac98843a062296545350bc39eedc23a09eff
Oakkar1998 <oakkarnyunt@gmail.com>
2025-08-06 22:49:06 +0630

    Finish Mini SPA

--- src/assets/pages/404.tsx
+++ src/assets/pages/404.tsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom"
 
 
 function NotFound() {
   return (
-    <div>404 Not Found.</div>
+    <>
+      <div className="flex grow text-center items-center justify-center">
+        <div className="p-8">
+          <p className="text-3xl md:text-5xl">404 - Page Not Found</p>
+          <p className="my-8 text-red-500 text-xl md:text-3xl">The page you are looking for does not exist!</p>
+          <Link to="/" className=" bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded">
+            Go back to Home
+          </Link>
+        </div>      
+      </div>    
+    </>
   )
 }
 
--- src/assets/pages/Cart.tsx
+++ src/assets/pages/Cart.tsx
@@ -2,7 +2,13 @@
 function Cart() {
   return (
     <>
-      <div className="flex grow">Cart</div>    
+      <div className="flex grow">
+        <div className="p-8">
+          <p className="text-5xl">Cart</p>
+        <p className="mt-8 text-red-500 text-3xl">Your Card is empty!</p> 
+        </div>
+      </div>
+      
     </>
   )
 }
